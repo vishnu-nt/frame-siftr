79827aff0d392d34604ffef32f77ef387ce82fc5
vishnu <vishnu.nt@outlook.com>
2026-06-18 18:44:20 +0530

    fix: disable posthog in local

--- src/index.tsx
+++ src/index.tsx
@@ -6,10 +6,12 @@ import reportWebVitals from './reportWebVitals';
 import posthog from 'posthog-js';
 import { PostHogProvider } from '@posthog/react';
 
-posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
-  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
-  defaults: '2026-01-30',
-});
+if (import.meta.env.PROD) {
+  posthog.init(import.meta.env.VITE_PUBLIC_POSTHOG_KEY, {
+    api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+    defaults: '2026-01-30',
+  });
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
